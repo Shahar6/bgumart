001a806bdaa81175abf531e05879b3218e110cae
Shahar6 <shahab4r@gmail.com>
2023-01-15 23:03:33 +0200

    initiate module

--- initiate.py
+++ initiate.py
@@ -3,40 +3,43 @@ from persistence import *
 import sys
 import os
 
-def add_branche(splittedline : list[str]):
-    #TODO: add the branch into the repo
-    pass
 
-def add_supplier(splittedline : list[str]):
-    #TODO: insert the supplier into the repo
-    pass
+def add_branch(splittedline: list[str]):
+    repo.insert_branch(splittedline)
 
-def add_product(splittedline : list[str]):
-    #TODO: insert product
-    pass
 
-def add_employee(splittedline : list[str]):
-    #TODO: insert employee
-    pass
+def add_supplier(splittedline: list[str]):
+    repo.insert_supplier(splittedline)
 
-adders = {  "B": add_branche,
-            "S": add_supplier,
-            "P": add_product,
-            "E": add_employee}
 
-def main(args : list[str]):
+def add_product(splittedline: list[str]):
+    repo.insert_product(splittedline)
+
+
+def add_employee(splittedline: list[str]):
+    repo.insert_employee(splittedline)
+
+
+adders = {"B": add_branch,
+          "S": add_supplier,
+          "P": add_product,
+          "E": add_employee}
+
+
+def main(args: list[str]):
     inputfilename = args[1]
     # delete the database file if it exists
     repo._close()
     # uncomment if needed
-    # if os.path.isfile("bgumart.db"):
-    #     os.remove("bgumart.db")
+    if os.path.isfile("bgumart.db"):
+        os.remove("bgumart.db")
     repo.__init__()
     repo.create_tables()
     with open(inputfilename) as inputfile:
         for line in inputfile:
-            splittedline : list[str] = line.strip().split(",")
+            splittedline: list[str] = line.strip().split(",")
             adders.get(splittedline[0])(splittedline[1:])
+
 
 if __name__ == '__main__':
     main(sys.argv)
--- persistence.py
+++ persistence.py
@@ -1,7 +1,8 @@
 import sqlite3
 import atexit
 from dbtools import Dao
- 
+
+
 # Data Transfer Objects:
 class Employee(object):
     def __init__(self, id, name, salary, branch):
@@ -9,12 +10,14 @@ class Employee(object):
         self.name = name
         self.salary = salary
         self.branch = branch
- 
+
+
 class Supplier(object):
     def __init__(self, id, name, contact_information):
         self.id = id
         self.name = name
         self.contact_information = contact_information
+
 
 class Product(object):
     def __init__(self, id, description, price, quantity):
@@ -23,11 +26,13 @@ class Product(object):
         self.price = price
         self.quantity = quantity
 
+
 class Branch(object):
     def __init__(self, id, location, number_of_employees):
         self.id = id
         self.location = location
         self.number_of_employees = number_of_employees
+
 
 class Activity(object):
     def __init__(self, product_id, quantity, activator_id, date):
@@ -35,9 +40,9 @@ class Activity(object):
         self.quantity = quantity
         self.activator_id = activator_id
         self.date = date
- 
- 
-#Repository
+
+
+# Repository
 class Repository(object):
     def __init__(self):
         self._conn = sqlite3.connect('bgumart.db')
@@ -47,11 +52,52 @@ class Repository(object):
         self.products = Dao(Product, self._conn)
         self.branches = Dao(Branch, self._conn)
         self.activities = Dao(Activity, self._conn)
- 
+
     def _close(self):
         self._conn.commit()
         self._conn.close()
- 
+    def get_employee_report(self):
+        c = self._conn.cursor()
+        return c.execute("""SELECT employees.name, employees.salary, branches.location 
+        FROM (employees INNER JOIN branches ON employees.branch = branches.id) ORDER BY employees.name ASC""").fetchall()
+
+    def get_all_activities(self):
+        c = self._conn.cursor()
+        return c.execute("SELECT * FROM activities ORDER BY activities.date ASC").fetchall()
+
+    def get_all_branches(self):
+        c = self._conn.cursor()
+        return c.execute("SELECT * FROM branches ORDER BY branches.id ASC").fetchall()
+
+    def get_all_employees(self):
+        c = self._conn.cursor()
+        return c.execute("SELECT * FROM employees ORDER BY employees.id ASC").fetchall()
+
+    def get_all_products(self):
+        c = self._conn.cursor()
+        return c.execute("SELECT * FROM products ORDER BY products.id ASC").fetchall()
+
+    def get_all_suppliers(self):
+        c = self._conn.cursor()
+        return c.execute("SELECT * FROM suppliers ORDER BY suppliers.id ASC").fetchall()
+
+    def insert_branch(self, splittedline: list[str]):
+        c = self._conn.cursor()
+        c.execute("INSERT INTO branches (id, location, number_of_employees) VALUES (?,?,?)",
+                  [splittedline[0], splittedline[1], splittedline[2]])
+    def insert_supplier(self, splittedline: list[str]):
+        c = self._conn.cursor()
+        c.execute("INSERT INTO suppliers (id, name, contact_information) VALUES (?,?,?)",
+                  [splittedline[0], splittedline[1], splittedline[2]])
+    def insert_product(self, splittedline: list[str]):
+        c = self._conn.cursor()
+        c.execute("INSERT INTO products (id, description, price, quantity) VALUES (?,?,?,?)",
+                  [splittedline[0], splittedline[1], splittedline[2], splittedline[3]])
+    def insert_employee(self, splittedline: list[str]):
+        c = self._conn.cursor()
+        c.execute("INSERT INTO employees (id, name, salary, branch) VALUES (?,?,?,?)",
+                  [splittedline[0], splittedline[1], splittedline[2], splittedline[3]])
+
     def create_tables(self):
         self._conn.executescript("""
             CREATE TABLE employees (
@@ -90,7 +136,8 @@ class Repository(object):
 
     def execute_command(self, script: str) -> list:
         return self._conn.cursor().execute(script).fetchall()
- 
+
+
 # singleton
 repo = Repository()
 atexit.register(repo._close)
